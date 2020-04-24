3c0162fa4f57fbf35c4beaa34bf557d185fee53d
Diego Romario <diego.romario@outlook.com>
2020-04-24 12:21:51 -0300

    finishing gRPC client (console)

--- src/gRPC.NETCore.Client/Program.cs
+++ src/gRPC.NETCore.Client/Program.cs
@@ -10,18 +10,26 @@ namespace gRPC.NETCore.Client
     class Program
     {
         static async Task Main()
-        {
+        {            
+            Console.WriteLine("Which stock would you like to subscribe to get quotes?\n");
+            string STOCK = Console.ReadLine();
+
             using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-
             var client = new StockListener.StockListenerClient(channel);
-
             try
             {
-                await foreach (var quote in client.GetQuotesStream(new Stock { Ticker = "GOOG" }).ResponseStream.ReadAllAsync())
+                await foreach (var quote in client.GetQuotesStream(new Stock { Ticker = STOCK }).ResponseStream.ReadAllAsync())
                 {
-                    var date = quote.Datetime;
-
-                    Console.WriteLine($"{quote.Ticker} = {date:s}");
+                    Console.WriteLine($"\nDate: {quote.Datetime:s}\nTicker: {quote.Ticker}");
+                    if (quote.Percentagechange.Contains("-"))
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    else
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine($"Quote: {quote.Quote}\nVariation Value: { quote.Variationvalue}\nPercentage Change: { quote.Percentagechange}");
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine($"Prior Quote: { quote.Priorquote}\n");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine($"{new String('-', 50)}");
                 }
             }
             catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
